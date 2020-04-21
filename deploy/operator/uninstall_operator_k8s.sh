7165514321e0da8f13b63f6874a3ebf92b956b94
nikhilbarge <niks.barge@gmail.com>
2020-04-22 03:18:29 +0530

    updated to ease uninstallation
    changes done so namespace need to set at only one place

--- deploy/operator/uninstall_operator_k8s.sh
+++ deploy/operator/uninstall_operator_k8s.sh
@@ -1,6 +1,10 @@
-kubectl delete -f infinibox-operator/deploy/crds/infinibox-csi-driver-service.yaml -n infi
-kubectl delete -f infinibox-operator/deploy/operator.yaml -n infi
-kubectl delete -f infinibox-operator/deploy/role_binding.yaml -n infi
-kubectl delete -f infinibox-operator/deploy/role.yaml -n infi
-kubectl delete -f infinibox-operator/deploy/service_account.yaml -n infi
-kubectl delete -f infinibox-operator/deploy/crds/infiniboxcsidriver_crd.yaml -n infi
+#! /bin/bash
+
+nmspace="infi"
+
+kubectl delete -f infinibox-operator/deploy/crds/infinibox-csi-driver-service.yaml -n $nmspace
+kubectl delete -f infinibox-operator/deploy/operator.yaml -n $nmspace
+kubectl delete -f infinibox-operator/deploy/role_binding.yaml -n $nmspace
+kubectl delete -f infinibox-operator/deploy/role.yaml -n $nmspace
+kubectl delete -f infinibox-operator/deploy/service_account.yaml -n $nmspace
+kubectl delete -f infinibox-operator/deploy/crds/infiniboxcsidriver_crd.yaml -n $nmspace
