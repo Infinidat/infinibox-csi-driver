daa25a1994d2b833949f6ff949bbe1021f6653aa
nikhilbarge <niks.barge@gmail.com>
2020-04-22 03:26:47 +0530

    updated to ease installation

--- deploy/operator/install_operator_k8s.sh
+++ deploy/operator/install_operator_k8s.sh
@@ -1,13 +1,17 @@
 #! /bin/bash
-kubectl get ns | grep infi 
+
+# set name space for operator here
+nmspace="infi"
+
+kubectl get ns | grep $nmspace 
 if [ $? -ne 0 ];
-  then echo "creating namespace"; kubectl create ns infi
+  then echo "creating namespace"; kubectl create ns $nmspace
 fi
 
 
-kubectl create -f infinibox-operator/deploy/crds/infiniboxcsidriver_crd.yaml -n infi
-kubectl create -f infinibox-operator/deploy/service_account.yaml -n infi
-kubectl create -f infinibox-operator/deploy/role.yaml -n infi
-kubectl create -f infinibox-operator/deploy/role_binding.yaml -n infi
-kubectl create -f infinibox-operator/deploy/operator.yaml -n infi
-kubectl create -f infinibox-operator/deploy/crds/infinibox-csi-driver-service.yaml -n infi
+kubectl create -f infinibox-operator/deploy/crds/infiniboxcsidriver_crd.yaml -n $nmspace
+kubectl create -f infinibox-operator/deploy/service_account.yaml -n $nmspace
+kubectl create -f infinibox-operator/deploy/role.yaml -n $nmspace
+kubectl create -f infinibox-operator/deploy/role_binding.yaml -n $nmspace
+kubectl create -f infinibox-operator/deploy/operator.yaml -n $nmspace
+kubectl create -f infinibox-operator/deploy/crds/infinibox-csi-driver-service.yaml -n $nmspace
