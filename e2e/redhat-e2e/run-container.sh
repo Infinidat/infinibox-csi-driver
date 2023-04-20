e234d300b2c1f898f27626122cf3d6dad5a1f6cb
jmccormick <jmccormick@infinidat.com>
2023-04-20 08:23:00 -0500

    add other protocols and test container version support into e2e

--- e2e/redhat-e2e/run-container.sh
+++ e2e/redhat-e2e/run-container.sh
@@ -16,6 +16,10 @@ if [[ -z "$_E2E_NAMESPACE" ]]; then
     echo "Must provide _E2E_NAMESPACE in environment" 1>&2
     exit 1
 fi
+if [[ -z "$_E2E_OCP_VERSION" ]]; then
+    echo "Must provide _E2E_OCP_VERSION in environment" 1>&2
+    exit 1
+fi
 if [[ -z "$OPTIONAL_DOCKER_BUILD_FLAGS" ]]; then
     echo "WARNING: for running tests on the VPN, you will want to provide OPTIONAL_DOCKER_BUILD_FLAGS in environment, example: --network=host" 1>&2
 fi
@@ -23,15 +27,13 @@ echo $_E2E_PROTOCOL is the protocol we are testing
 
 echo $_E2E_NAMESPACE is e2e namespace
 
-VERSION=v4.11
-
 # create working dir
 export WORKDIR=`mktemp -d /tmp/redhat-e2e.$_E2E_PROTOCOL.XXXXXX`
 echo $WORKDIR
 
 echo $KUBECONFIG was kubeconfig at start
 
-BINDIR=/tmp/redhat-e2e/$VERSION
+BINDIR=/tmp/redhat-e2e/$_E2E_OCP_VERSION
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
@@ -44,7 +46,7 @@ envsubst < $SCRIPT_DIR/e2e-volume-snapshotclass.yaml > $WORKDIR/e2e-volume-snaps
 
 export TEST_CSI_DRIVER_FILES=$WORKDIR/e2e-manifest-$_E2E_PROTOCOL.yaml 
 echo $OPTIONAL_DOCKER_BUILD_FLAGS are the docker flags
-docker run  $OPTIONAL_DOCKER_BUILD_FLAGS -v $WORKDIR:/data:z --rm -it registry.redhat.io/openshift4/ose-tests:$VERSION sh -c "KUBECONFIG=/data/kubeconfig.yaml TEST_CSI_DRIVER_FILES=/data/e2e-manifest.yaml /usr/bin/openshift-tests run openshift/csi --junit-dir /data/results"
+docker run  $OPTIONAL_DOCKER_BUILD_FLAGS -v $WORKDIR:/data:z --rm -it registry.redhat.io/openshift4/ose-tests:$_E2E_OCP_VERSION sh -c "KUBECONFIG=/data/kubeconfig.yaml TEST_CSI_DRIVER_FILES=/data/e2e-manifest.yaml /usr/bin/openshift-tests run openshift/csi --junit-dir /data/results"
 
 # example:  to run a set of tests
 #docker run  $OPTIONAL_DOCKER_BUILD_FLAGS -v $WORKDIR:/data:z --rm -it registry.redhat.io/openshift4/ose-tests:v4.11 sh -c "KUBECONFIG=/data/kubeconfig.yaml TEST_CSI_DRIVER_FILES=/data/e2e-manifest-nfs.yaml /usr/bin/openshift-tests run openshift/csi --file /data/tests-to-run --junit-dir /data/results_nfs_all"
