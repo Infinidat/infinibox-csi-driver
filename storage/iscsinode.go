cb81464380d4c3e5916728323383a4f230167ca2
jeffmccormick <jmccormick@infinidat.com>
2023-08-27 15:34:39 -0500

    fix spelling of iscsi mutual_chap constant to match the expected value

--- storage/iscsinode.go
+++ storage/iscsinode.go
@@ -88,8 +88,9 @@ type SessionDetails struct {
 }
 
 const (
-	USE_CHAP            = "chap"
-	USE_CHAP_MUTUAL     = "chap_mutual"
+	USE_CHAP = "chap"
+	//USE_CHAP_MUTUAL     = "chap_mutual"
+	USE_CHAP_MUTUAL     = "mutual_chap"
 	ISCSI_TCP_TRANSPORT = "tcp"
 	CHAP_USERNAME       = "node.session.auth.username"
 	CHAP_PASSWORD       = "node.session.auth.password"
@@ -584,7 +585,7 @@ func (iscsi *iscsistorage) AttachDisk(b iscsiDiskMounter) (mntPath string, err e
 			}
 
 			// zlog.Debug().Msgf("Wait for iscsi device path: %s to appear", devicePath)
-			timeout := 10
+			timeout := 30
 			if devExists := iscsi.waitForPathToExist(&devicePath, timeout, iscsiTransport); devExists {
 				zlog.Debug().Msgf("iscsi device path found: %s", devicePath)
 				devicePaths = append(devicePaths, devicePath)
