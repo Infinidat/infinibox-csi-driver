0747887bc90321512805022335dc882a5ee52b51
jeffmccormick <jmccormick@infinidat.com>
2023-10-31 15:56:02 -0500

    CSIC-622 bump up sidecar image versions

--- e2e/external-e2e/run.sh
+++ e2e/external-e2e/run.sh
@@ -50,6 +50,7 @@ envsubst < $SCRIPT_DIR/e2e-volume-snapshotclass.yaml > $WORKDIR/e2e-volume-snaps
 	--ginkgo.skip="ephemeral" \
 	--ginkgo.skip="Ephemeral" \
 	--ginkgo.skip="access to two volumes" \
+	--ginkgo.skip="OnRootMismatch" \
 	--ginkgo.skip="when restoring snapshot to larger size pvc" \
 	-storage.testdriver=$WORKDIR/e2e-manifest.yaml \
 	> $WORKDIR/results.log
