a32c5ae9563f8ca7582542a04989eae726514e18
Jeff McCormick <jmccormick@infinidat.com>
2025-03-18 19:32:51 +0000

    CSIC-496 fix SAST issues and semgrep issues

--- cmd/infinidat-csi-metrics/main.go
+++ cmd/infinidat-csi-metrics/main.go
@@ -6,6 +6,7 @@ import (
 	metric "infinibox-csi-driver/metrics"
 	"net/http"
 	"os"
+	"time"
 
 	"infinibox-csi-driver/log"
 
@@ -62,7 +63,44 @@ func main() {
 
 	http.Handle("/", &home{})
 	http.Handle("/metrics", promhttp.Handler())
-	_ = http.ListenAndServe(":"+*metric.PortFlag, nil)
+	//_ = http.ListenAndServe(":"+*metric.PortFlag, nil)
+	srv := &http.Server{
+		Addr: ":" + *metric.PortFlag,
+		// ReadHeaderTimeout is the amount of time allowed to read
+		// request headers. The connection's read deadline is reset
+		// after reading the headers and the Handler can decide what
+		// is considered too slow for the body. If ReadHeaderTimeout
+		// is zero, the value of ReadTimeout is used. If both are
+		// zero, there is no timeout.
+		ReadHeaderTimeout: 15 * time.Second,
+
+		// ReadTimeout is the maximum duration for reading the entire
+		// request, including the body. A zero or negative value means
+		// there will be no timeout.
+		//
+		// Because ReadTimeout does not let Handlers make per-request
+		// decisions on each request body's acceptable deadline or
+		// upload rate, most users will prefer to use
+		// ReadHeaderTimeout. It is valid to use them both.
+		ReadTimeout: 15 * time.Second,
+
+		// WriteTimeout is the maximum duration before timing out
+		// writes of the response. It is reset whenever a new
+		// request's header is read. Like ReadTimeout, it does not
+		// let Handlers make decisions on a per-request basis.
+		// A zero or negative value means there will be no timeout.
+		WriteTimeout: 10 * time.Second,
+
+		// IdleTimeout is the maximum amount of time to wait for the
+		// next request when keep-alives are enabled. If IdleTimeout
+		// is zero, the value of ReadTimeout is used. If both are
+		// zero, there is no timeout.
+		IdleTimeout: 30 * time.Second,
+	}
+	if err := srv.ListenAndServe(); err != nil {
+		zlog.Info().Msgf("fatal error on srv.ListenAndServe %s", err.Error())
+		os.Exit(1)
+	}
 }
 
 type home struct{}
