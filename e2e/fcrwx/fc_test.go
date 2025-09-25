fbef26ab5da1a585df4c9bcbec66e37c69af36aa
jmccormick <jmccormick@infinidat.com>
2025-09-25 10:11:38 -0500

    CSIC-915 move fc rwx test into its own e2e test target, removing it from the fc test target

---
+++ e2e/fcrwx/fc_test.go
@@ -0,0 +1,59 @@
+//go:build e2e
+
+package fcrwx
+
+import (
+	"infinibox-csi-driver/common"
+	"infinibox-csi-driver/e2e"
+	"testing"
+	"time"
+
+	v1 "k8s.io/api/core/v1"
+)
+
+func TestFcBlockRWX(t *testing.T) {
+
+	testConfig, err := e2e.GetTestConfig(t, common.PROTOCOL_FC)
+	if err != nil {
+		t.Fatalf("error getting TestConfig %s\n", err.Error())
+	}
+
+	nodeCount := e2e.GetTestSystemNodecount(t, testConfig.ClientSet)
+
+	if nodeCount < 2 {
+		t.Fatalf("System needs at least 2 nodes and only has %d ", nodeCount)
+	}
+
+	testConfig.AccessMode = v1.ReadWriteMany
+	testConfig.UseAntiAffinity = true
+	testConfig.UseBlock = true
+
+	e2e.Setup(testConfig)
+
+	time.Sleep(10)
+
+	firstSuccess, _, err := e2e.VerifyBlockWriteInPod(testConfig.ClientSet, testConfig.RestConfig, e2e.POD_NAME, testConfig.TestNames.NSName)
+	if err != nil {
+		t.Fatalf("Verify Block Write In Pod had unexpected error %s", err.Error())
+	}
+
+	if !firstSuccess {
+		t.Fatalf("Test of Blockwrite in %s pod failed.", e2e.POD_NAME)
+	}
+
+	secondSuccess, _, err := e2e.VerifyBlockWriteInPod(testConfig.ClientSet, testConfig.RestConfig, e2e.ANTI_AF_POD_NAME, testConfig.TestNames.NSName)
+	if err != nil {
+		t.Fatalf("Verify Block Write In Pod had unexpected error %s", err.Error())
+	}
+
+	if !secondSuccess {
+		t.Fatalf("Test of Blockwrite in %s pod failed.", e2e.POD_NAME)
+	}
+
+	if *e2e.CleanUp {
+		e2e.TearDown(testConfig)
+	} else {
+		t.Logf("not cleaning up namespace")
+	}
+
+}
