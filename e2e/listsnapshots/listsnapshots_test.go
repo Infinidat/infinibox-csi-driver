0ebee73272e8b773995d22ceec6c268c44eb11bd
jmccormick <jmccormick@infinidat.com>
2026-07-07 15:02:12 -0500

    add listsnapshots

---
+++ e2e/listsnapshots/listsnapshots_test.go
@@ -0,0 +1,68 @@
+//go:build e2e
+
+/*
+Copyright 2026 Infinidat
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package listsnapshots
+
+import (
+	"context"
+	"testing"
+
+	"github.com/container-storage-interface/spec/lib/go/csi"
+	"github.com/infinidat/infinibox-csi-driver/api/clientgo"
+	"github.com/infinidat/infinibox-csi-driver/common"
+	"github.com/infinidat/infinibox-csi-driver/e2e"
+	"github.com/infinidat/infinibox-csi-driver/service"
+)
+
+func TestIscsiListSnapshots(t *testing.T) {
+
+	testConfig, err := e2e.GetTestConfig(t, common.ProtocolISCSI)
+	if err != nil {
+		t.Fatalf("error getting TestConfig %s\n", err.Error())
+	}
+
+	e2e.Setup(t.Context(), testConfig)
+
+	// TODO need to create a snapshot here
+
+	cl, err := clientgo.BuildOffClusterClient(e2e.KubeConfigPath)
+	if err != nil {
+		t.Fatalf("error getting kube client %s\n", err.Error())
+	}
+
+	// call ListVolumes and make sure a volume exists
+	req := &csi.ListSnapshotsRequest{}
+
+	//resp, err := service.ListSnapshotsImplementation(context.Background(), req, cl, e2e.E2E_NAMESPACE)
+	resp, err := service.ListSnapshotsImplementation(context.Background(), req, cl, "infinidat-csi")
+	if err != nil {
+		t.Fatalf("error calling ListSnapshotsImpl %s\n", err.Error())
+	}
+	if len(resp.Entries) < 1 {
+		t.Fatal("error expected at least 1 volume from ListSnapshots")
+	}
+	t.Logf("got volumes %d from ListSnapshots call\n", len(resp.Entries))
+
+	if *e2e.CleanUp {
+		e2e.TearDown(t.Context(), testConfig)
+	} else {
+		t.Log("not cleaning up namespace")
+	}
+	err = e2e.CleanISCI(t.Context(), *testConfig)
+	if err != nil {
+		t.Logf("error cleaning ISCSI %s on node %s\n", err.Error(), testConfig.NodeName)
+	}
+
+}
--- service/controller.go
+++ service/controller.go
@@ -20,7 +20,6 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/infinidat/infinibox-csi-driver/api"
 	"github.com/infinidat/infinibox-csi-driver/api/clientgo"
@@ -32,7 +31,6 @@ import (
 	"github.com/infinidat/infinibox-csi-driver/storage/nvme"
 
 	"github.com/container-storage-interface/spec/lib/go/csi"
-	tspb "google.golang.org/protobuf/types/known/timestamppb"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -623,10 +621,6 @@ func (s *ControllerServer) ListVolumes(ctx context.Context, req *csi.ListVolumes
 func (s *ControllerServer) ListSnapshots(ctx context.Context, req *csi.ListSnapshotsRequest) (*csi.ListSnapshotsResponse, error) {
 	slog.Info("Started", "MaxEntries", req.MaxEntries)
 
-	res := &csi.ListSnapshotsResponse{
-		Entries: make([]*csi.ListSnapshotsResponse_Entry, 0),
-	}
-
 	// Get a k8s go client for in-cluster use
 	client, err := clientgo.BuildClient()
 	if err != nil {
@@ -643,114 +637,11 @@ func (s *ControllerServer) ListSnapshots(ctx context.Context, req *csi.ListSnaps
 		return nil, status.Error(codes.Unavailable, msg)
 	}
 
-	secrets, err := client.GetSecrets(ctx, namespace)
+	res, err := ListSnapshotsImplementation(ctx, req, client, namespace)
 	if err != nil {
-		e := common.Errorf("GetSecrets - error: %w", err)
+		e := common.Errorf("ListSnapshotsImpl - error: %w", err)
 		slog.Error(e.Error())
 		return nil, status.Error(codes.Unavailable, e.Error())
-	}
-
-	for _, secret := range secrets {
-		client := api.ClientService{
-			ConfigMap:  make(map[string]string),
-			SecretsMap: secret,
-		}
-
-		clientService, err := client.NewClient()
-		if err != nil {
-			e := common.Errorf("NewClient - error: %w", err)
-			slog.Error(e.Error())
-			return nil, status.Error(codes.Unavailable, e.Error())
-		}
-
-		snapshots, err := clientService.IboxAPI.GetAllSnapshots(ctx)
-		if err != nil {
-			e := common.Errorf("GetAllSnapshots - error: %w", err)
-			slog.Error(e.Error())
-			return nil, status.Error(codes.Unavailable, e.Error())
-		}
-		slog.Debug("got back", "snapshots", len(snapshots))
-
-		// handle the optional case where a SnapshotId is passed in the ListSnapshots request
-		var volumeInfo api.VolumeProtocolConfig
-		var volumeID int
-		if req.SnapshotId != "" {
-			volumeInfo, err = storagecommon.ValidateVolumeID(req.SnapshotId)
-			if err != nil {
-				e := common.Errorf("ValidateVolumeID - error: %w", err)
-				slog.Error(e.Error())
-				return nil, status.Error(codes.InvalidArgument, e.Error())
-			}
-			volumeID = volumeInfo.VolumeID
-		}
-
-		for _, snapshot := range snapshots {
-			createdAtValue := snapshot.CreatedAt / 1000
-			timeValue := time.Unix(createdAtValue, 0)
-			timestampValue := tspb.New(timeValue)
-
-			var parentName string
-			slog.Log(ctx, common.LevelTrace, "info", "snapshot datasettype", snapshot.DatasetType)
-			switch snapshot.DatasetType {
-			case "VOLUME":
-				_, err := clientService.IboxAPI.GetVolume(ctx, snapshot.ParentID)
-				if err != nil {
-					e := common.Errorf("GetVolume - snapshot error name %s parentID %d error %w", snapshot.Name, snapshot.ParentID, err)
-					slog.Error(e.Error())
-					parentName = UNKNOWN
-				} else {
-					parentName = strconv.Itoa(snapshot.ParentID)
-				}
-			case "FILESYSTEM":
-				_, err := clientService.IboxAPI.GetFileSystemByID(ctx, snapshot.ParentID)
-				if err != nil {
-					e := common.Errorf("GetFileSystemByID - snapshot error name %s parentid %d error %w", snapshot.Name, snapshot.ParentID, err)
-					slog.Error(e.Error())
-					parentName = UNKNOWN
-				} else {
-					parentName = strconv.Itoa(snapshot.ParentID)
-				}
-			default:
-				slog.Error("snapshot unknown dataset type", "name", snapshot.Name, "datasettype", snapshot.DatasetType, "parentid", snapshot.ParentID)
-				parentName = UNKNOWN
-			}
-
-			entry := csi.ListSnapshotsResponse_Entry{
-				Snapshot: &csi.Snapshot{
-					SnapshotId:     snapshot.Name,
-					SourceVolumeId: parentName,
-					SizeBytes:      snapshot.Size,
-					CreationTime:   timestampValue,
-					ReadyToUse:     true, // always true on the ibox according to Jason.
-				},
-			}
-
-			slog.Log(ctx, common.LevelTrace, "info", "source volume id", req.SourceVolumeId, "snapshot id", req.SnapshotId)
-
-			if req.SourceVolumeId != "" {
-				volumeInfo, err := storagecommon.ValidateVolumeID(req.SourceVolumeId)
-				if err != nil {
-					e := common.Errorf("ValidateVolumeID - error validating sourceVolumeId %s %w", req.SourceVolumeId, err)
-					slog.Error(e.Error())
-					return nil, status.Error(codes.InvalidArgument, e.Error())
-				}
-				slog.Log(ctx, common.LevelTrace, "comparing", "volume id", volumeInfo.VolumeID, "source volume id", entry.Snapshot.SourceVolumeId, "snapshot", entry.Snapshot)
-				if strconv.Itoa(volumeInfo.VolumeID) == entry.Snapshot.SourceVolumeId {
-					slog.Log(ctx, common.LevelTrace, "matches!")
-					entry.Snapshot.SourceVolumeId = req.SourceVolumeId // set the SourceVolumeId sent back to the incoming format xxxx$$nfs
-					res.Entries = append(res.Entries, &entry)
-				}
-			} else if req.SnapshotId != "" {
-				slog.Log(ctx, common.LevelTrace, "comparing", "volumeid", volumeID, "snapshot id", snapshot.ID)
-				if volumeID == snapshot.ID {
-					slog.Log(ctx, common.LevelTrace, "req.SnapshotID contains found matching snapshot with ID and name", "snapshot id", req.SnapshotId, "snapshot id 2", snapshot.ID, "name", snapshot.Name)
-					entry.Snapshot.SnapshotId = req.SnapshotId
-					res.Entries = append(res.Entries, &entry)
-				}
-			} else {
-				res.Entries = append(res.Entries, &entry)
-			}
-		}
 	}
 
 	return res, nil
